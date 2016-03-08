81fcb9cdfcafb35a19cdba9454020e744385adc7
Aaron Sinnott <Aaron Sinnott>
2016-03-08 11:23:48 +0000

    Settings fix

--- reusable_blog/models.py
+++ reusable_blog/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 class Post(models.Model):
     # author is linked to a registered user in the "auth_user" table
-    author = models.ForeignKey(settings.AUTH_MODEL)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(
