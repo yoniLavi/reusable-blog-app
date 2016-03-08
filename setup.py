267329da294de827c906ba8d4111f3a701edc3e4
Aaron Sinnott <Aaron Sinnott>
2016-03-08 11:07:52 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readmme.read()
+    README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
