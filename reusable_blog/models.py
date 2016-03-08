993168b751c410daada2ab24dbf5bb85c8f6e37e
Aaron Sinnott <Aaron Sinnott>
2016-03-08 11:17:15 +0000

    User relationship fix

--- reusable_blog/models.py
+++ reusable_blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 
 class Post(models.Model):
     # author is linked to a registered user in the "auth_user" table
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(settings.AUTH_MODEL)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(
