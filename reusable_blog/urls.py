4f9042da2631f4e3696f76277714cfae9c7a927e
Yoni Lavi <yoni@codeinstitute.net>
2017-06-06 18:28:53 +0100

    Remove unnecessary slashes
    Below are the warnings that appeared with the code prior to this change.
    
    WARNINGS:
    ?: (urls.W002) Your URL pattern '^/$' [name='post_list'] has a regex beginning with a '/'. Remove this slash as it is unnecessary. If this pattern is targeted in an include(), ensure the include() pattern has a trailing '/'.
    ?: (urls.W002) Your URL pattern '^/stuff/$' [name='post_list'] has a regex beginning with a '/'. Remove this slash as it is unnecessary. If this pattern is targeted in an include(), ensure the include() pattern has a trailing '/'.

--- reusable_blog/urls.py
+++ reusable_blog/urls.py
@@ -2,11 +2,8 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-
     url(r'^$', views.post_list, name="post_list"),
-    url(r'^/$', views.post_list, name="post_list"),
-    url(r'^/stuff/$', views.post_list, name="post_list"),
+    url(r'^stuff/$', views.post_list, name="post_list"),
     url(r'^(?P<id>\d+)/$', views.post_detail),
     url(r'^post/$', views.new_post, name='new_post'),
-
 ]
